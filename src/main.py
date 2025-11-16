dfa6916c485f1c21d0c2502dcdfa92259f29fdbc
enzoaccioly1 <146199749+enzoaccioly1@users.noreply.github.com>
2025-11-16 19:11:33 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import sqlparse
 from components.text_to_sql_pipeline import TextToSQLPipeline
 
-# Estabelece a conexão com o banco de dados.
+# Estabelece a conexão com o banco de dados
 def connectionSQL():
     try:
         cnx = psycopg2.connect(
@@ -14,7 +14,7 @@ def connectionSQL():
             port='5432',
             database='testesDB',
             user='postgres',
-            password='teste123'
+            password='master'
         )
         print("Conexão com o PostgreSQL estabelecida com sucesso.")
 
@@ -37,7 +37,7 @@ def connectionSQL():
         print("Erro ao conectar ao PostgreSQL:", e)
         return None
 
-# Implementação do menu. 
+# Implementação do menu
 def menu():
     print("-----------------------\n Menu (Acervo Digital) \n-----------------------")
     print("1. Criar Tabelas (Pronto) \n2. Carregar Tabelas (Pronto) \n3. Atualizar Tabelas (Pronto) \n4. Consultar Tabelas (Pronto) \n5. Deletar Tabelas (Pronto) \n6. CRUD \n7. Inserção \n8. Atualização \n9. Exclusão \n10. Consulta \n11. Consulta em Linguagem Natural \n0. Sair")
@@ -46,7 +46,7 @@ def menu():
             opcao = int(input("Selecione uma opção válida: "))
     return opcao
 
-# Executa o comando(DDL) conforme o caminho do arquivo sql.
+# Executa o comando (DDL) conforme o caminho do arquivo sql
 def executarSQL(connect, sqlCaminho):
     # Lê o script SQL completo
     sql = Path(sqlCaminho).read_text(encoding="utf-8")
@@ -62,32 +62,39 @@ def executarSQL(connect, sqlCaminho):
         print("-------------------------------------------")
         print("ERRO AO EXECUTAR O SCRIPT!")
         print("O banco de dados fez um ROLLBACK automático.")
-        print(f"Mensagem do PostgreSQL: {e.pgerror}")
+        print(e.pgerror)
         print("-------------------------------------------")
-        # raise faz com que a função que chamou executarSQL também receba a exceção e não continue a execução normalmente.
+        # 'raise' faz com que a função que chamou executarSQL também receba a exceção e não continue a execução normalmente
         raise 
     finally:
         cursor.close()
 
-# Executa manipulações(DML) conforme o caminho do arquivo sql.
+# Executa manipulações (DML) conforme o caminho do arquivo sql
 def consultarSQL(connect, sqlCaminho):
     sql = Path(sqlCaminho).read_text(encoding="utf-8")
     
     try:
         cursor = connect.cursor()
         cursor.execute(sql)
-        resultado = cursor.fetchall()
-        return resultado
+        connect.commit()
+
+        try:
+            resultado = cursor.fetchall()
+            return resultado
+        except: 
+            return None
     
     except psycopg2.Error as e:
-        print("Erro ao executar SELECT!")
+        print("-------------------------------------------")
+        print("ERRO AO EXECUTAR O SCRIPT!")
         print(e.pgerror)
+        print("-------------------------------------------")
         raise
     
     finally:
         cursor.close()
     
-# Carrega o esquema completo no banco. 
+# Carrega o esquema completo no banco
 def criarTabelas(connect):
     sqlCaminho = Path(__file__).parent.parent / "sql" / "schema.sql"
     executarSQL(connect, sqlCaminho)
@@ -105,7 +112,7 @@ def atualizarTabelas(connect):
     executarSQL(connect, sqlCaminho)
     print("Valores atualizados no banco!\n")
 
-# Realiza consulta pré-definida. 
+# Realiza consulta pré-definida
 def consultarTabelas(connect):
     sqlCaminho = Path(__file__).parent.parent / "sql" / "queries.sql"
     resultado = consultarSQL(connect, sqlCaminho)
@@ -114,11 +121,66 @@ def consultarTabelas(connect):
     for linha in resultado:
         print(linha)
         
-# Deleta todas as tabelas do banco.
+# Deleta todas as tabelas do banco
 def deletarTabelas(connect):
     sqlCaminho = Path(__file__).parent.parent / "sql" / "dropCascade.sql"
     executarSQL(connect, sqlCaminho)
     print("Esquema deletado com sucesso!\n")
+
+def mostrarTabelas(connect):
+    cursor = connect.cursor()
+    # Listar todas as tabelas no schema public
+    cursor.execute("""
+        SELECT table_name 
+        FROM information_schema.tables 
+        WHERE table_schema = 'public';
+    """)
+    tables = [row[0] for row in cursor.fetchall()]
+
+    for tableName in tables:
+        # Listar todas as colunas em cada tabela
+        cursor.execute("""
+            SELECT column_name
+            FROM information_schema.columns
+            WHERE table_schema = 'public'
+            AND table_name = %s
+            ORDER BY ordinal_position;
+        """, (tableName,))
+
+        columnNames = [row[0] for row in cursor.fetchall()]
+
+        # Imprima o resultado
+        print(f"{tableName}: {columnNames}")
+    cursor.close()
+
+# Coloca o código SQL dentro do arquivo para chamar a função de DML
+def varSQL(connect, sql):
+    sqlCaminho = Path(__file__).parent.parent / "sql" / "variable.sql"
+    sqlCaminho.write_text(sql)
+    resultado = consultarSQL(connect, sqlCaminho)
+
+    try:
+        for linha in resultado:
+            print(linha)
+    except:
+        print("Ação executada com sucesso.")
+
+def atualizar(connect):
+    mostrarTabelas(connect)
+    
+    tableName = input(str("\nSelecione uma tabela para atualizar: ")).upper()
+    select = "select * from " + tableName
+    varSQL(connect, select)
+            
+    atb = input("Digite o atributo a ser alterado: ")
+    valor = input("Digite o valor a ser atribuído: ")
+    if isinstance(valor, str): # Testa se é string
+        valor = f"'{valor}'"
+    col = input("Digite o nome da coluna da chave primária: ")
+    id = input("Digite o valor numérico do campo da chave primária: ")
+    query = ['UPDATE ', tableName, ' SET ', atb, ' = ', valor, ' WHERE ', col, ' = ', id]
+    sql = ''.join(query)
+    varSQL(connect, sql)
 
 def executarPipeline(connect, descricao):
     sqlCaminho = Path(__file__).parent.parent / "sql" / "schema.sql"
@@ -153,11 +215,16 @@ def main():
             case 5:
                 deletarTabelas(conn)
             case 6:
-                break
+                deletarTabelas(conn)
+                criarTabelas(conn)
+                carregarTabelas(conn)
+                atualizarTabelas(conn)
+                consultarTabelas(conn)
+                deletarTabelas(conn)
             case 7:
                 break
             case 8:
-                break
+                atualizar(conn)
             case 9:
                 break
             case 10:
@@ -172,5 +239,3 @@ def main():
                     
 if __name__ == "__main__":
     main()
-    
-
