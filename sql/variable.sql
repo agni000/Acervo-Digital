d4b2405e0ae019d438bd71abdf9f3440da05f0a6
Júnior Pires <j.juniorpires98@gmail.com>
2025-11-21 11:41:36 -0300

    chore: atualizar descricao do readme

--- sql/variable.sql
+++ sql/variable.sql
@@ -1,1 +1,7 @@
-UPDATE PESSOA SET nome = 'Bia Nunes' WHERE id_pessoa = 9
+-- Retorna o total de producoes filtrado por Editora
+SELECT E.nome, COUNT(P.id_producao) AS total_producoes 
+FROM PRODUCAO P 
+JOIN PRODUCAO_EDITORA PE ON P.id_producao = PE.id_producao 
+JOIN EDITORA E ON PE.id_editora = E.id_editora 
+GROUP BY E.nome 
+ORDER BY total_producoes;
