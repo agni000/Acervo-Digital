517250dc74576b7dd2e3e92ae49054f2723c858b
enzoaccioly1 <146199749+enzoaccioly1@users.noreply.github.com>
2025-11-24 22:10:02 -0300

    Update variable.sql

--- sql/variable.sql
+++ sql/variable.sql
@@ -1,7 +1,7 @@
 -- Retorna o total de producoes filtrado por Editora
 SELECT E.nome, COUNT(P.id_producao) AS total_producoes 
 FROM PRODUCAO P 
-JOIN PRODUCAO_EDITORA PE ON P.id_producao = PE.id_producao 
-JOIN EDITORA E ON PE.id_editora = E.id_editora 
+JOIN PUBLICACAO PU ON P.id_producao = PU.id_producao 
+JOIN EDITORA E ON PU.id_editora = E.id_editora 
 GROUP BY E.nome 
 ORDER BY total_producoes;
